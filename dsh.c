fb978795f4ed1205568080710bf8d340a17cf105
Luis Sagastume <luis@ayalo.co>
2015-01-18 19:36:20 -0600

    Fisrt commit

---
+++ dsh.c
@@ -0,0 +1,14 @@
+/*
+* Author:Luis Sagastume
+* Email:lsagastume1990@gmail.com
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "dsh.h"
+
+int main(int argc, char **argv)
+{
+  run_shell();
+}
+
