bd6b07c37c2eb8ceedb35b0bd25e463a585a7632
Luis Sagastume <luis@ayalo.co>
2015-02-19 23:31:21 -0600

    Added exit status

--- dsh.c
+++ dsh.c
@@ -1,14 +1,18 @@
 /*
 * Author:Luis Sagastume
 * Email:lsagastume1990@gmail.com
+* DSH: a little shell for learning purposes
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "lib/dsh.h"
 
+#define EXIT 0
+
 int main(int argc, char **argv)
 {
   run_shell();
+  return EXIT;
 }
 
